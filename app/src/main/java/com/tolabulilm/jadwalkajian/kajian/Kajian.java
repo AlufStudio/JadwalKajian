5c3890a26ab0f35ef3eb3cf6a1856c306519db76
azhargen <sendtofata@live.com>
2017-01-21 03:20:23 -0500

    Update kajian model

--- app/src/main/java/com/tolabulilm/jadwalkajian/kajian/Kajian.java
+++ app/src/main/java/com/tolabulilm/jadwalkajian/kajian/Kajian.java
@@ -16,4 +16,79 @@ public class Kajian {
     private String status;
     private String hijri;
     private String contactNumber;
+
+    public Kajian() {
+
+    }
+
+    public Kajian(String id, String ustadz, String title, String place, long time,
+    String city, String type) {
+        this.id = id;
+        this.ustadz = ustadz;
+        this.title = title;
+        this.place = place;
+        this.time = time;
+        this.city = city;
+        this.type = type;
+    }
+
+    public void setUstadz(String ustadz) {
+        this.ustadz = ustadz;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setHijri(String hijri) {
+        this.hijri = hijri;
+    }
+
+    public void setContactNumber(String number) {
+        this.contactNumber = number;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getUstadz() {
+        return this.ustadz;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public long getTime() {
+        return this.time;
+    }
 }
