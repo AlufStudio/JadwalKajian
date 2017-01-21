9d5b26c8a58f50649af11f613c4640213ef9954c
azhargen <sendtofata@live.com>
2017-01-20 20:55:04 -0500

    Add variables to add kajian activity, add a method to check empty inputs

--- app/src/main/java/com/tolabulilm/jadwalkajian/kajian/AddKajianActivity.java
+++ app/src/main/java/com/tolabulilm/jadwalkajian/kajian/AddKajianActivity.java
@@ -6,10 +6,31 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.tolabulilm.jadwalkajian.R;
 
 public class AddKajianActivity extends AppCompatActivity {
+
+    private EditText inputUstadz;
+    private EditText inputTitle;
+    private EditText inputPlace;
+    private EditText inputAddress;
+    private EditText inputHijri;
+    private EditText inputContactNumber;
+
+    private Button dateButton;
+    private Button hourButton;
+
+    private String ustadz;
+    private String title;
+    private String place;
+    private String address;
+    private String hijri;
+    private String contactNumber;
+
+    private boolean inputIsEmpty = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,4 +40,13 @@ public class AddKajianActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
+    private void checkEmptyInput() {
+        if(inputUstadz.getText().toString().isEmpty() || inputTitle.getText().toString().isEmpty()) {
+            inputIsEmpty = true;
+        } else if (inputPlace.getText().toString().isEmpty()) {
+            inputIsEmpty = true;
+        } else {
+            inputIsEmpty = false;
+        }
+    }
 }
