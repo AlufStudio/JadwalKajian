33789fb5813c1caf52a581a489b0ff71be8d68b7
Unknown <sendtofata@live.com>
2017-02-03 13:49:54 +0700

    Display donasi list

--- app/src/main/java/com/tolabulilm/jadwalkajian/donasi/InfoDonasiActivity.java
+++ app/src/main/java/com/tolabulilm/jadwalkajian/donasi/InfoDonasiActivity.java
@@ -3,6 +3,8 @@ package com.tolabulilm.jadwalkajian.donasi;
 public class InfoDonasiActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private DatabaseReference donasiRef;
+    private FirebaseRecyclerAdapter firebaseAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -11,6 +13,31 @@ public class InfoDonasiActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        initView();
+        initFirebase();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void initView() {
+        recyclerView = (RecyclerView)findViewById(R.id.info_donasi_recycler);
+    }
+
+    private void initFirebase() {
+        donasiRef = FirebaseDatabase.getInstance().getReference("donasi");
+        firebaseAdapter = new FirebaseRecyclerAdapter<Donasi, DonasiViewHolder> (
+            Donasi.class,
+            R.layout.card_donasi,
+            DonasiViewHolder.class,
+            donasiRef
+        ) {
+            @Override
+            protected void populateViewHolder(DonasiViewHolder viewHolder, Donasi model, int position) {
+                viewHolder.setView(model);
+                viewHolder.setProgressBar(model.getDonated, model.getNeeded);
+            }
+        };
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(firebaseAdapter);
     }
 }
